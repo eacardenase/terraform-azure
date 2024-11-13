e4d956a8308c6f81481a98eece19855f2878f3fe
Edwin Cardenas <eacardenase@gmail.com>
2024-11-13 00:56:31 +0000

    feat: add network security group and rule resources

--- main.tf
+++ main.tf
@@ -36,3 +36,27 @@ resource "azurerm_subnet" "mtc_subnet" {
   virtual_network_name = azurerm_virtual_network.mtc_vn.name
   address_prefixes     = ["10.123.1.0/24"]
 }
+
+resource "azurerm_network_security_group" "mtc_nsg" {
+  name                = "mtc_nsg"
+  resource_group_name = azurerm_resource_group.mtc_rg.name
+  location            = azurerm_resource_group.mtc_rg.location
+
+  tags = {
+    "environment" = "dev"
+  }
+}
+
+resource "azurerm_network_security_rule" "mtc_dev_rule" {
+  name                        = "mtc_dev_rule"
+  priority                    = 100
+  direction                   = "Inbound"
+  access                      = "Allow"
+  protocol                    = "*"
+  source_port_range           = "*"
+  destination_port_range      = "*"
+  source_address_prefix       = "*"
+  destination_address_prefix  = "*"
+  resource_group_name         = azurerm_resource_group.mtc_rg.name
+  network_security_group_name = azurerm_network_security_group.mtc_nsg.name
+}
