f98abcc5881a589f365d67910d4ec5f6f82f0dfd
Edwin Cardenas <eacardenase@gmail.com>
2024-11-13 01:48:12 +0000

    feat: add network interface card

--- main.tf
+++ main.tf
@@ -76,3 +76,20 @@ resource "azurerm_public_ip" "mtc_ip" {
     environment = "dev"
   }
 }
+
+resource "azurerm_network_interface" "mtc_nic" {
+  name                = "mtc_nic"
+  location            = azurerm_resource_group.mtc_rg.location
+  resource_group_name = azurerm_resource_group.mtc_rg.name
+
+  ip_configuration {
+    name                          = "internal"
+    private_ip_address_allocation = "Dynamic"
+    subnet_id                     = azurerm_subnet.mtc_subnet.id
+    public_ip_address_id          = azurerm_public_ip.mtc_ip.id
+  }
+
+  tags = {
+    environment = "dev"
+  }
+}
