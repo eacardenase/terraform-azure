4489142c1079fc4063bfbb9cd7c10c464cfa12fb
Edwin Cardenas <eacardenase@gmail.com>
2024-11-17 01:19:24 +0000

    feat: conditionally set provisioner interpreter

--- main.tf
+++ main.tf
@@ -130,7 +130,7 @@ resource "azurerm_linux_virtual_machine" "mtc_vm" {
       user         = "adminuser"
       identityfile = "~/.ssh/mtcazurekey"
     })
-    interpreter = ["bash", "-c"]
+    interpreter = var.host_os == "linux" ? ["bash", "-c"] : ["Powershell", "-Command"]
   }
 
   tags = {
